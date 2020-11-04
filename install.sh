7f9d50278b0ff8b68ce9c6c2ba313a073020c699
nasoskal <thanasiskal81@gmail.com>
2020-11-04 10:50:18 +0200

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/vim-easymotion
+
+git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
+
+git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
+
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+
+cp _vimrc ../.vimrc
