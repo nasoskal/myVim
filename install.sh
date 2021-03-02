fb38d00421b90d408311a25a1aed065117433793
nasoskal <thanasiskal81@gmail.com>
2021-03-02 22:18:48 +0200

    Added CoC and Auto Pairs functionality

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt install curl
+sudo curl -sL install-node.now.sh/lts | sudo bash
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
@@ -14,6 +17,5 @@ git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
 
 git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim 
-
 
 cp .vimrc ~/.vimrc
